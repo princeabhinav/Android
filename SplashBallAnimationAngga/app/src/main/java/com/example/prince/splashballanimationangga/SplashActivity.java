09387aab9c1b44686db83083f566f06f6a593857
princeabhinav <prince262150@gmail.com>
2018-01-12 17:47:17 +0530

    animation

---
+++ SplashBallAnimationAngga/app/src/main/java/com/example/prince/splashballanimationangga/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.prince.splashballanimationangga;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    Button sub;
+    ImageView ballon ;
+    Animation frombottom, fromtop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        sub=(Button)findViewById(R.id.sub);
+        ballon=(ImageView)findViewById(R.id.ballon);
+        frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+        sub.setAnimation(frombottom);
+        ballon.setAnimation(fromtop);
+    }
+}
